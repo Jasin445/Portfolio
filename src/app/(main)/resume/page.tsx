5e3235446557684fc11c54bbfe851df943fa51f3
Jason Dagana <daganajason72@gmail.com>
2025-06-21 01:26:53 +0100

    added placeholder for resume

--- src/app/(main)/resume/page.tsx
+++ src/app/(main)/resume/page.tsx
@@ -1,7 +1,12 @@
+import BackgroundImg from "@/components/backgroundImg"
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <div className="h-screen flex justify-center items-center text-white">
+      <h1 className="text-5xl">Coming soon</h1>
+      </div>
+    </div>
   )
 }
 
